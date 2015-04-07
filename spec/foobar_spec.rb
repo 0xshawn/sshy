5d49ddf0f055bdc4dbecb953883bff76421aadc2
Shown Tien <hightian@gmail.com>
2015-04-07 23:12:36 +0800

    have a big forward

---
+++ spec/foobar_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Sshy do
+  pending "This is a test spec tester"
+  puts Sshy::VERSION
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'bundler/setup'
+Bundler.setup
+
+require_relative "../lib/sshy"
+#require '../lib/sshy' # and any other gems you need
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
